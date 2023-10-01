8d77c2325baa4626377b309213a460883d41c960
Amy <amy@litdevs.org>
2023-10-01 23:54:40 +0300

    Text/JSON support

--- README.md
+++ README.md
@@ -25,6 +25,7 @@ image.src = dataUrl
 nyaFile.queueCache("assets/someAsset")
 nyaFile.queueCache("assets/someAsset2")
 nyaFile.queueCache("assets/someAsset3")
+nyaFile.queueCache("assets/someJson")
 await nyaFile.waitAllCached()
 
 // We can now synchronously get the data url, for example to return an img element directly
@@ -32,11 +33,16 @@ return <img src={nyaFile.getCachedDataUrl("assets/someAsset")} />
 
 // Loading a new nyafile refreshes the cache
 await nyaFile.load("https://lightquark.network/not-not-default.nya")
-return <img src={nyaFile.getCachedDataUrl("assets/someAsset")} />
+return <img src={nyaFile.getCachedData("assets/someAsset")} />
 // Returns different value than before
+
+// Get some parsed JSON that was cached earlier
+let json = nyaFile.getCachedJson("assets/someJson")
+// Or load one that was not cached
+let newJson = await nyaFile.getAssetJson("assets/someOtherJson")
+// Or a text file
+let newText = await nyaFile.getAssetText("assets/someText")
+// This got cached in the previous operation so it can be retrieved
+// Notice we are using getCachedData for text
+let alreadyCachedText = nyaFile.getCachedData("assets/someText")
 ```
-
-### Caveats
-
-- Only getAssetDataUrl is implemented. More development needed for things like the \<NyaAsset> React component
-- Only certain file types are recognized
--- src/NyaFile.js
+++ src/NyaFile.js
@@ -5,20 +5,35 @@ export default class NyaFile {
 
     defaultFile;
     nyaFile;
-    dataUrlCache;
+    /**
+     * Cache entries are objects with 2 fields
+     * - kind -> which kind of asset is this? dataUrl for images/audio/video etc. text for text/json etc.
+     * - data -> relevant data or promise for getting it
+     */
+    assetCache;
     constructor () {
         if (NyaFile._instance) return NyaFile._instance;
         NyaFile._instance = this;
-        this.dataUrlCache = {}
+        this.assetCache = {}
     }
 
     /**
      * Ask to cache an asset for future use
      * This should ideally be called during initial load AFTER loading a custom nyafile for assets that are going to be used
      * @param filePath
+     * @param kind What kind of asset this is (dataUrl, text)
      */
-    queueCache(filePath) {
-        this.dataUrlCache[filePath] = this.getAssetDataUrl(filePath, true)
+    queueCache(filePath, kind = "dataUrl") {
+        let getDataMethod;
+        switch (kind) {
+            case "dataUrl":
+                getDataMethod = this.getAssetDataUrl.bind(this);
+                break;
+            case "text":
+                getDataMethod = this.getAssetText.bind(this);
+                break;
+        }
+        this.assetCache[filePath] = {kind: kind, data: getDataMethod(filePath, true)}
     }
 
     /**
@@ -26,16 +41,16 @@ export default class NyaFile {
      * @return {Promise<void>}
      */
     async waitAllCached() {
-        await Promise.all(Object.values(this.dataUrlCache))
+        await Promise.all(Object.values(this.assetCache).map(v => v.data))
     }
 
     /**
      * Performs a full cache refresh
      */
     async explodeCache() {
-        console.log(`Full refreshing cache with ${Object.keys(this.dataUrlCache).length} assets`)
-        for (const cachedAsset of Object.keys(this.dataUrlCache)) {
-            this.queueCache(cachedAsset)
+        console.log(`Full refreshing cache with ${Object.keys(this.assetCache).length} assets`)
+        for (const cachedAsset of Object.keys(this.assetCache)) {
+            this.queueCache(cachedAsset, this.assetCache[cachedAsset].kind)
         }
         //console.log("All entries re-queued")
         await this.waitAllCached()
@@ -104,26 +119,69 @@ export default class NyaFile {
      * @return {Promise<string>}
      */
     async getAssetDataUrl(filePath, ignoreCache = false) {
-        if (!ignoreCache && this.dataUrlCache[filePath]) {
+        if (!ignoreCache && this.assetCache[filePath]) {
             //console.log(`Returning cached asset for ${filePath}`)
-            return this.dataUrlCache[filePath];
+            return await this.assetCache[filePath].data;
         }
         let assetFile = await this.getFile(filePath);
         let assetBase64 = await assetFile.async("base64");
         let dataUrl = `data:${nameToMime(assetFile.name)};base64,${assetBase64}`;
-        if (ignoreCache || !this.dataUrlCache[filePath]) this.dataUrlCache[filePath] = dataUrl;
+        if (ignoreCache || !this.assetCache[filePath]) this.assetCache[filePath] = {kind: "dataUrl", data: dataUrl};
         //console.log(`${filePath} added to cache`)
         return dataUrl
     }
 
     /**
+     * getAssetDataUrl for text based files
+     * @param filePath
+     * @param ignoreCache
+     * @return {Promise<*>}
+     */
+    async getAssetText(filePath, ignoreCache = false) {
+        if (!ignoreCache && this.assetCache[filePath]) {
+            //console.log(`Returning cached asset for ${filePath}`)
+            return await this.assetCache[filePath].data;
+        }
+        let assetFile = await this.getFile(filePath);
+        let assetText = await assetFile.async("text");
+        if (ignoreCache || !this.assetCache[filePath]) this.assetCache[filePath] = {kind: "text", data: assetText};
+        //console.log(`${filePath} added to cache`)
+        return assetText;
+    }
+
+    /**
+     * getAssetDataUrl for JSON
+     * @param filePath
+     * @param ignoreCache
+     * @return {Promise<*>}
+     */
+    async getAssetJson(filePath, ignoreCache = false) {
+        return JSON.parse(await this.getAssetText(filePath, ignoreCache));
+    }
+
+    /**
      * If you are sure an asset is cached you can use this method to get the url synchronously
+     * If the asset is not cached you will have a bad time
+     * Use this method for cached data urls or text
      * @param filePath
      * @return {string}
      */
-    getCachedDataUrl(filePath) {
-        if (!this.dataUrlCache[filePath]) throw new Error(`Asset ${filePath} is not cached, but tried to get with getCachedDataUrl()`)
-        //console.log(`Returning cached asset for ${filePath}`)
-        return this.dataUrlCache[filePath]
+    getCachedData(filePath) {
+        if (!this.assetCache[filePath]) throw new Error(`getCachedData Tried to retrieve asset ${filePath} from cache but not cached`)
+        return this.assetCache[filePath].data;
+    }
+
+    /**
+     * If you are sure an asset is cached you can use this method to get the url synchronously
+     * If the asset is not cached you will have a bad time
+     * Use this method for cached JSON
+     * @param filePath
+     * @return {any}
+     */
+    getCachedJson(filePath) {
+        if (!this.assetCache[filePath]) throw new Error(`getCachedJson Tried to retrieve asset ${filePath} from cache but not cached`)
+        console.log(filePath)
+        console.log(this.assetCache[filePath])
+        return JSON.parse(this.assetCache[filePath].data);
     }
 }
