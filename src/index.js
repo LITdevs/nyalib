cd416984458b12d1d1bafb13aa3b641367e0c784
Amy <amy@litdevs.org>
2023-10-15 14:53:56 +0300

    No more default import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import NyaFile from "./NyaFile.js";
 import NyaSoundClickable from "./components/NyaSoundClickable.jsx";
 
-export default NyaFile;
-
-export {NyaSoundClickable}
+export {NyaSoundClickable, NyaFile}
