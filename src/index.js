bc2eaa04981aa107bfda475494b1440bc7ada869
Amy <amy@litdevs.org>
2023-10-15 17:05:08 +0300

    Export StyleProvider

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import NyaFile from "./NyaFile.js";
 import NyaSoundClickable from "./components/NyaSoundClickable.jsx";
+import StyleProvider from "./components/StyleProvider.jsx";
 
 export default NyaFile;
-export {NyaSoundClickable, NyaFile}
+export {NyaSoundClickable, StyleProvider, NyaFile}
